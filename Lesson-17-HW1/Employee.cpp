1db41d30120bba59993cfeb378b552e469dfc2cc
Timofey Sadovnikov <timofeysadovnikov2009@gmail.com>
2024-01-15 22:16:58 +0200

    Methods definition

--- Lesson-17-HW1/Employee.cpp
+++ Lesson-17-HW1/Employee.cpp
@@ -1,1 +1,57 @@
 #include "Employee.h"
+
+Employee::Employee()
+    : lastName(""), birthYear(0), position(""), salary(0.0), education("") {}
+
+Employee::Employee(string lastName, int birthYear, string position, double salary, string education)
+    : lastName(lastName), birthYear(birthYear), position(position), salary(salary), education(education) {}
+
+Employee::~Employee() {}
+
+string Employee::getLastName() const {
+    return lastName;
+}
+
+void Employee::setLastName(string lastName) {
+    this->lastName = lastName;
+}
+
+int Employee::getBirthYear() const {
+    return birthYear;
+}
+
+void Employee::setBirthYear(int birthYear) {
+    this->birthYear = birthYear;
+}
+
+string Employee::getPosition() const {
+    return position;
+}
+
+void Employee::setPosition(string position) {
+    this->position = position;
+}
+
+double Employee::getSalary() const {
+    return salary;
+}
+
+void Employee::setSalary(double salary) {
+    this->salary = salary;
+}
+
+string Employee::getEducation() const {
+    return education;
+}
+
+void Employee::setEducation(string education) {
+    this->education = education;
+}
+
+void Employee::displayInfo() const {
+    cout << "Last Name: " << lastName << endl;
+    cout << "Birth Year: " << birthYear << endl;
+    cout << "Position: " << position << endl;
+    cout << "Salary: " << salary << endl;
+    cout << "Education: " << education << endl;
+}
